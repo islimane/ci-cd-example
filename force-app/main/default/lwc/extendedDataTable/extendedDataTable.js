a7c9246df5348647e7fe5dadc829b3ca0eb560db
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2025-01-29 21:29:16 +0100

    (rateManager) BCRMB2B-18 Scrollable table v1

---
+++ force-app/main/default/lwc/extendedDataTable/extendedDataTable.js
@@ -0,0 +1,68 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class FixedColumnsTable extends LightningElement {
+    @api
+    get columns() {
+        return this._columns;
+    }
+    set columns(value) {
+        this._columns = !!value ? value : [];
+        this.splitColumns();
+    }
+
+    @api tableData = [];
+    @api fixedColumnCount = 0;
+
+    @track _columns = [];
+    @track fixedColumns = [];
+    @track scrollableColumns = [];
+    @track fixedTableData = [];
+    @track scrollableTableData = [];
+    @track leftCSSProperty = 0;
+
+    connectedCallback() {
+        this.splitColumns();
+        this.setLeftProperty();
+        this.splitTableData();
+    }
+
+    renderedCallback() {
+        let scrollableTable = this.template.querySelector('.scrollable-table lightning-datatable');
+        if (scrollableTable) {
+            scrollableTable.style.left = `${this.leftCSSProperty}px`;
+            scrollableTable.style.position = `absolute`;
+        }
+    }
+
+    splitColumns() {
+        this.splitScrollableColumns();
+        this.splitFixedColumns();
+    }
+
+    splitScrollableColumns() {
+        this.scrollableColumns = JSON.parse(JSON.stringify(this._columns.slice(this.fixedColumnCount)));
+    }
+
+    splitFixedColumns() {
+        let _fixedColumns = this._columns.slice(0, this.fixedColumnCount);
+        _fixedColumns.forEach(column => {
+            if(column.wrapText){
+                this.scrollableColumns.unshift(column);
+            }
+        });
+        this.fixedColumns = JSON.parse(JSON.stringify(_fixedColumns));
+    }
+
+    setLeftProperty() {
+        this.fixedColumns.forEach(column => {
+            if(column.wrapText){
+                this.leftCSSProperty -= column.fixedWidth;
+            }
+        });
+    }
+
+    splitTableData() {
+        this.fixedTableData = this.tableData;
+        this.scrollableTableData = this.tableData;
+    }
+}
