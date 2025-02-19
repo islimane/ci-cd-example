155fb99137a730b8fe62f54b69e011e4338f2b63
alberto.martinez-lopez <alberto.martinez-lopez@inetum.com>
2025-02-19 14:28:14 +0100

    save method

--- force-app/main/default/lwc/lwcDCExtension/lwcDCExtension.js
+++ force-app/main/default/lwc/lwcDCExtension/lwcDCExtension.js
@@ -2,10 +2,11 @@
  * @description  : 
  * @author       : development@nubika.com 
 **/
-import { LightningElement} from 'lwc';
+import { LightningElement, api} from 'lwc';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import * as DCMixin from 'c/dcMixin';
 import { NavigationMixin } from 'lightning/navigation';
+
 
 export default class LwcDCExtension extends 
     DCMixin.LWCFormMixin(
@@ -15,6 +16,11 @@ export default class LwcDCExtension extends
                             NavigationMixin(LightningElement)
     )))){ 
     
+    /**
+     * @description Loads Lightning Web Components resources (styles and scripts) to the LWC component
+     * @param {Object} resources - object containing styles and scripts urls to be loaded
+     * @returns {Promise} - resolves when all resources has been loaded
+     */
     loadResources(resources){
         try{
             let self = this;
@@ -29,7 +35,31 @@ export default class LwcDCExtension extends
         }catch(e){ return new Promise((resolve,reject)=>{ reject(e); }); }
     }
 
+    /**
+     * Parses an object by converting it to JSON and then back to an object. This is useful for creating a shallow copy of an object.
+     * @param {Object} obj - the object to be parsed
+     * @returns {Object} the parsed object
+     */
     parseObject(obj){
         return obj? JSON.parse(JSON.stringify(obj)) : obj;
+    }
+
+    @api
+    set recordId(value){
+        this._recordId = value;
+    }
+    get recordId(){
+        return this._recordId;
+    } 
+
+    parentId;
+    editMode = false;
+
+    /**
+     * Save the record.
+     * @abstract
+     */
+    save(){
+        
     }
 }
