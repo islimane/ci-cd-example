552ca0ad0103ffae409e25593ee0c5b3d6c362f0
alvaro.marin <alvaro.marin@nubika.com>
2025-03-06 11:35:58 +0100

    BCRMB2B-16: add lwc and controllers (pending)

--- force-app/main/default/lwc/rateManagerEventList/rateManagerEventList.js
+++ force-app/main/default/lwc/rateManagerEventList/rateManagerEventList.js
@@ -7,10 +7,87 @@
  * @see          : 
 **/
 import LwcDCExtension from 'c/lwcDCExtension';
+import { track, api } from 'lwc';
 import { RateManagerMixin } from 'c/rateManagerMixin';
 import LABELS from './labels.js';
+import rateManagerModalPeriodHandler from 'c/rateManagerModalPeriodHandler';
+
+export default class RateManagerPeriodList extends RateManagerMixin(LwcDCExtension) {
+	labels = LABELS;
+
+	connectedCallback(){
+		this._wireParams = {recordId: this.parentId, controller: 'RateManagerEventListController'};
+	}   
+
+	fetch = (response) => {
+		this._eventList = JSON.parse(JSON.stringify(response?.data)) || response.data;
+	}
+
+	@track
+	_eventList = [];
 
 
-export default class RateManagerEventList extends RateManagerMixin(LwcDCExtension) {
-    labels = LABELS;
+	get eventList(){
+		// let eventList = this._eventList.map((Id, index) => {
+		// 	return {
+		// 		...Id,
+		// 		index
+		// 	}
+		// })
+		// return eventList;
+		return [];
+	}
+
+	/**
+	 * @description Saves the period list by invoking the save method.
+	 *              Logs the local name of the template host element upon saving.
+	 */
+
+	@api
+	handleSave(){
+		this.save(() => {
+		   console.log('@@@ Save Periods', this.template.host.localName);
+		});
+	}
+
+
+	async handleAddEventModal() {
+
+		const result = await rateManagerModalPeriodHandler.open({
+			// it is set on lightning-modal-header instead
+			dateIntervals: this._eventList,
+			parentId: this.parentId,
+			size: 'large',
+			headerLabel: this.labels.addEvent,
+			onconfirm: (e) => {
+				// stop further propagation of the event
+				e.stopPropagation();
+				this.handleAddEvent(e);
+			}
+		});
+
+		console.log(result);
+	}
+
+	handleAddEvent(event){
+		this.refreshFetch();
+	}
+
+	handleRefreshEvent(event){
+		this.refreshFetch();
+	}
+
+	async handleDeleteEvent(event){
+		try{
+			const result = await this.remoteAction({
+				controller: 'RateManagerPeriodListController',
+				action: 'deletePeriod',
+				recordIds: [event.detail.recordId],
+			});
+			this.refreshFetch();
+		}catch(e){
+			console.error(e.message);
+		}
+		
+	}
 }
