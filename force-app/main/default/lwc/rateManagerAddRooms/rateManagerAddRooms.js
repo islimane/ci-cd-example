3442ecb33ec1c3642081b5cea43a3c024c64e0c3
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-03-25 08:52:38 +0100

    feat(BCRMB2B-21) Add Room Controller

--- force-app/main/default/lwc/rateManagerAddRooms/rateManagerAddRooms.js
+++ force-app/main/default/lwc/rateManagerAddRooms/rateManagerAddRooms.js
@@ -1,8 +1,14 @@
-import {LightningElement,track, api} from 'lwc';
+/**
+ * @description       :
+ * @author            : Inetum Team <alberto.martinez-lopez@inetum.com>
+ * @group             :
+ * @last modified on  : 24-03-2025
+ * @last modified by  : Inetum Team <alberto.martinez-lopez@inetum.com>
+**/
+import { LightningElement, track, api } from 'lwc';
 import LwcDCExtension from 'c/lwcDCExtension';
 
 export default class RateManagerAddRooms extends LwcDCExtension {
-
     @api parentId;
     @track filters = [];
     @track _tableData = [];
@@ -11,19 +17,20 @@ export default class RateManagerAddRooms extends LwcDCExtension {
     set tableData(value) {
         const data = [];
         if (value) {
-            value.forEach(record => {
+            value.forEach((record) => {
                 let row = {};
-                this.columns.forEach(column => {
+                this.columns.forEach((column) => {
                     row[column.fieldName] = record[column.fieldName] || null;
                 });
+                row.Id = record.Id;
+                row.Hotel__c = record.Hotel__c;
                 data.push(row);
             });
             this._tableData = data;
         }
-        this._tableData  = JSON.parse(JSON.stringify(data)) || data;
+        this._tableData = JSON.parse(JSON.stringify(data)) || data;
         this.filteredData = [...this._tableData];
     }
-
 
     get tableData() {
         return this._tableData;
@@ -31,13 +38,14 @@ export default class RateManagerAddRooms extends LwcDCExtension {
 
     get columns() {
         return [
-            { label: 'HABITACIÓN', fieldName: 'Room__c', type: 'text'},
+            { label: 'NOMBRE', fieldName: 'Name', type: 'text' },
+            { label: 'HABITACIÓN', fieldName: 'Room__c', type: 'text' },
             { label: 'CARACTERÍSTICA', fieldName: 'Characteristic__c', type: 'text' }
         ];
     }
 
     /*** Connected callback.*/
-    connectedCallback(){
+    connectedCallback() {
         this.setWireParams();
     }
 
@@ -45,7 +53,7 @@ export default class RateManagerAddRooms extends LwcDCExtension {
      * @description: Sets the wire parameters for the component.
      **/
     setWireParams() {
-        this._wireParams = {parentId: this.parentId, controller: 'RateManagerAddRoomController' };
+        this._wireParams = { parentId: this.parentId, controller: 'RateManagerAddRoomController' };
     }
 
     /**
@@ -57,12 +65,12 @@ export default class RateManagerAddRooms extends LwcDCExtension {
         if (fetchedRecords) {
             this.filters = response.data.filters;
             this.tableData = response.data.data;
-            console.log('SARA_filters: '+ JSON.stringify(this.filters));
-            console.log('SARA_tableData: '+ JSON.stringify(this.tableData));
+            console.log('SARA_filters: ' + JSON.stringify(this.filters));
+            console.log('SARA_tableData: ' + JSON.stringify(this.tableData));
         } else {
             console.warn('No records available in response');
         }
-    }
+    };
 
     /**
      * Handles the change of a filter value
@@ -72,22 +80,23 @@ export default class RateManagerAddRooms extends LwcDCExtension {
         try {
             const activeFilters = event.detail;
             if (activeFilters.length === 0) {
-                this.filteredData = [...this._tableData]
-                return
+                this.filteredData = [...this._tableData];
+                return;
             }
             // Start with all data
             this.filteredData = this._tableData.filter((record) => {
                 // Record must pass ALL filters to be included
                 return activeFilters.every((filter) => {
-                    return record[filter.fieldApiName] === filter.value
-                })
+                    return record[filter.fieldApiName] === filter.value;
+                });
             });
         } catch (e) {
             console.error(e.message);
         }
     }
 
-
-
-
+    @api
+    getSelectedRows() {
+        return this.template.querySelector('lightning-datatable').getSelectedRows();
+    }
 }
