d63a21e1118351118c59a98bc727e0c71a5a5b8a
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-03-25 13:30:14 +0100

    feat(BCRMB2B-21): DataTableManager listen event refresh table

--- force-app/main/default/lwc/extendedDataTableManager/extendedDataTableManager.js
+++ force-app/main/default/lwc/extendedDataTableManager/extendedDataTableManager.js
@@ -1,18 +1,17 @@
 /**
- * @description       :
+ * @description       : Shows a table with fixed columns and scrollable columns. Includes also a filter component and action buttons
  * @author            : Inetum Team <alberto.martinez-lopez@inetum.com>
  * @group             :
- * @last modified on  : 19-03-2025
- * @last modified by  : Inetum Team <sara.gerico@inetum.com>
-**/
-import { LightningElement,api,track } from 'lwc';
+ * @last modified on  : 25-03-2025
+ * @last modified by  : Inetum Team <ruben.sanchez-gonzalez@inetum.com>
+ **/
+import { LightningElement, api, track } from 'lwc';
 import LABELS from './labels';
 import rateManagerModalRoomHandler from 'c/rateManagerModalRoomHandler';
 import LwcDCExtension from 'c/lwcDCExtension';
 import { RateManagerMixin } from 'c/rateManagerMixin';
 
 export default class ExtendedDataTableManager extends RateManagerMixin(LwcDCExtension) {
-
     labels = LABELS;
 
     @api filters;
@@ -30,23 +29,20 @@ export default class ExtendedDataTableManager extends RateManagerMixin(LwcDCExte
         return this.flag ? 'currency' : 'number';
     }
 
-
-
-
     @api
     set tableData(value) {
         const data = [];
         if (value) {
-            value.forEach(record => {
+            value.forEach((record) => {
                 let row = {};
-                this.columns.forEach(column => {
+                this.columns.forEach((column) => {
                     row[column.fieldName] = record[column.fieldName] || null;
                 });
                 data.push(row);
             });
             this._tableData = data;
         }
-        this._tableData  = JSON.parse(JSON.stringify(data)) || data;
+        this._tableData = JSON.parse(JSON.stringify(data)) || data;
         this.filteredData = [...this._tableData];
     }
 
@@ -62,39 +58,35 @@ export default class ExtendedDataTableManager extends RateManagerMixin(LwcDCExte
         try {
             const activeFilters = event.detail;
             if (activeFilters.length === 0) {
-                this.filteredData = [...this._tableData]
-                return
+                this.filteredData = [...this._tableData];
+                return;
             }
             // Start with all data
             this.filteredData = this._tableData.filter((record) => {
                 // Record must pass ALL filters to be included
                 return activeFilters.every((filter) => {
-                    return record[filter.fieldApiName] === filter.value
-                })
+                    return record[filter.fieldApiName] === filter.value;
+                });
             });
         } catch (e) {
             console.error(e.message);
         }
     }
 
-    //SARA//
-
     async handleAddRoomModal() {
-        const result = await rateManagerModalRoomHandler.open({
+        await rateManagerModalRoomHandler.open({
             parentId: this.parentId,
             size: 'large',
-            headerLabel: "Add Rooms",
-            onconfirm: (e) => {
+            headerLabel: 'Add Rooms',
+            onrefreshtable: (e) => {
                 e.stopPropagation();
-                this.handleAddRoom(e);
+                this.notifyParent(e);
             }
         });
     }
 
-    handleAddRoom(event){
-        this.refreshFetch();
+    notifyParent() {
+        console.log(`Refresh recibido en ${this.constructor.name}`);
+        this.dispatchEvent(new CustomEvent('refreshtable'));
     }
-
-
-
 }
