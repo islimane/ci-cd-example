3ca912cefa23c7d5842d193ffb7dbf72357cab52
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-03-31 15:25:34 +0200

    feat(BCRMB2B-22): rateManager modal component for supl/reductions

---
+++ force-app/main/default/lwc/rateManagerModalSupplementsAndDiscountsHandler/labels.js
@@ -0,0 +1,10 @@
+import add from '@salesforce/label/c.Add';
+import cancel from '@salesforce/label/c.Cancel';
+import noRecordsSelected from '@salesforce/label/c.No_records_selected';
+import save from '@salesforce/label/c.Save';
+export default {
+    add,
+    cancel,
+    noRecordsSelected,
+    save,
+};
