29aa113d98568c0c4fe26bfaac838d50f7842f4f
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-03-31 15:25:15 +0200

    feat(BCRMB2B-22): main component datatable for showing data of supp/disc

--- force-app/main/default/lwc/rateManagerRoomsConfig/rateManagerRoomsConfig.js
+++ force-app/main/default/lwc/rateManagerRoomsConfig/rateManagerRoomsConfig.js
@@ -1,13 +1,18 @@
 /**
- * @description       :
+ * @description       : Enables adding Rooms to a Rate Planner.
  * @author            : Inetum Team <alberto.martinez-lopez@inetum.com>
  * @group             :
- * @last modified on  : 28-03-2025
- * @last modified by  : alberto.martinez-lopez@inetum.com
+ * @last modified on  : 31-03-2025
+ * @last modified by  : Inetum Team <ruben.sanchez-gonzalez@inetum.com>
  **/
 import { api, track } from 'lwc';
 import LwcDCExtension from 'c/lwcDCExtension';
 import { RateManagerMixin } from 'c/rateManagerMixin';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { deleteRecord } from 'lightning/uiRecordApi';
+import LightningConfirm from 'lightning/confirm';
+import LABELS from './labels';
+import rateManagerModalRoomHandler from 'c/rateManagerModalRoomHandler';
 
 export default class RateManagerRoomsConfig extends RateManagerMixin(LwcDCExtension) {
     @api rateId;
@@ -28,6 +33,8 @@ export default class RateManagerRoomsConfig extends RateManagerMixin(LwcDCExtens
         return this._columns.filter((column) => column.fixed).length;
     }
 
+    labels = LABELS;
+
     /*** Connected callback.*/
     connectedCallback() {
         this.setWireParams();
@@ -37,7 +44,11 @@ export default class RateManagerRoomsConfig extends RateManagerMixin(LwcDCExtens
      * @description: Sets the wire parameters for the component.
      **/
     setWireParams() {
-        this._wireParams = { ratePlannerId: this.parentId, rateId: this.rateId, controller: 'RateManagerRoomsConfigController' };
+        this._wireParams = {
+            ratePlannerId: this.parentId,
+            rateId: this.rateId,
+            controller: 'RateManagerRoomsConfigController'
+        };
     }
 
     /**
@@ -53,34 +64,35 @@ export default class RateManagerRoomsConfig extends RateManagerMixin(LwcDCExtens
         } else {
             console.warn('No records available in response');
         }
-    }
+    };
 
-    buildTable(){
-        this._columns = [{ label: 'ACTIONS', fieldName: 'action', type: 'checkbox', fixed: true, fixedWidth: 109 },
+    buildTable() {
+        this._columns = [
             { label: 'NAME', fieldName: 'Name', type: 'text', fixed: true, fixedWidth: 200, wrapText: true },
             { label: 'ROOM', fieldName: 'Room', type: 'text', fixed: true, fixedWidth: 100, wrapText: true },
             { label: 'CHARACTERSITIC', fieldName: 'Characteristic', type: 'text', fixed: true, fixedWidth: 200, wrapText: true },
             { label: 'APPLICABLE', fieldName: 'Applicable', type: 'text', fixed: true, fixedWidth: 114 },
             { label: 'REGIMEN', fieldName: 'RegimenType', type: 'text', fixed: true, fixedWidth: 101 },
-            { label: 'AVG', fieldName: 'avg', type: 'currency', fixed: true, fixedWidth: 68 }];
+            { label: 'AVG', fieldName: 'avg', type: 'currency', fixed: true, fixedWidth: 68 }
+        ];
 
         const periods = new Set();
-        this.data.forEach(item => {
-            item.ratesPrices.forEach(rate => {
+        this.data.forEach((item) => {
+            item.ratesPrices.forEach((rate) => {
                 if (rate.StartDate && rate.EndDate) {
                     periods.add(rate.periodKey);
                 }
             });
         });
 
-        periods.forEach(period => {
+        periods.forEach((period) => {
             this._columns.push({ label: period, fieldName: period, type: 'currency', editable: true, fixedWidth: 200 });
         });
 
-        this.data = this.data.map(item => {
+        this.data = this.data.map((item) => {
             const newItem = { ...item };
-            periods.forEach(period => {
-                const rate = item.ratesPrices.find(element => element.periodKey === period);
+            periods.forEach((period) => {
+                const rate = item.ratesPrices.find((element) => element.periodKey === period);
                 newItem[period] = rate ? rate.TotalPrice : null;
             });
             return newItem;
@@ -91,15 +103,12 @@ export default class RateManagerRoomsConfig extends RateManagerMixin(LwcDCExtens
         this.refreshFetch();
     }
 
-    async handleSave(event){
-
+    async handleSave(event) {
         const draftValues = event.detail.draftValues;
         const mappedData = [];
-        draftValues.forEach(item => {
-            const dateRangeKeys = Object.keys(item).filter(key => 
-                key.includes('-') && /\d+\/\d+\/\d+/.test(key)
-            );
-            dateRangeKeys.forEach(dateKey => {
+        draftValues.forEach((item) => {
+            const dateRangeKeys = Object.keys(item).filter((key) => key.includes('-') && /\d+\/\d+\/\d+/.test(key));
+            dateRangeKeys.forEach((dateKey) => {
                 mappedData.push({
                     periodKey: dateKey,
                     TotalPrice: parseFloat(item[dateKey]),
@@ -108,16 +117,73 @@ export default class RateManagerRoomsConfig extends RateManagerMixin(LwcDCExtens
             });
         });
 
-        try{
+        try {
             const result = await this.remoteAction({
                 controller: 'RateManagerRoomsConfigController',
                 action: 'saveRatePrices',
                 ratePlannerId: this.parentId,
-                ratePrices: JSON.stringify(mappedData),
+                ratePrices: JSON.stringify(mappedData)
             });
-            if(result)this.refreshFetch();
-        }catch(e){
+            if (result) this.refreshFetch();
+        } catch (e) {
             console.error(e);
         }
+    }
+
+    async handleAddRoomModal() {
+        await rateManagerModalRoomHandler.open({
+            parentId: this.parentId,
+            size: 'large',
+            headerLabel: 'Add Rooms',
+            onrefreshtable: (e) => {
+                e.stopPropagation();
+                this.refreshTable();
+            }
+        });
+    }
+
+    async handleDelete() {
+        // retrieve selected rows
+        let selectedRows = this.template.querySelector('c-extended-data-table-manager')?.getSelectedRows();
+        console.log('Selected rows --> ' + selectedRows);
+        if (!selectedRows || selectedRows.length === 0) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    variant: 'Error',
+                    message: this.labels.noRecordsSelected
+                })
+            );
+            return;
+        }
+        const result = await LightningConfirm.open({
+            message: this.labels.removeConfirmation,
+            variant: 'headerless',
+            label: 'this is the aria-label value'
+        });
+
+        if (result) {
+            const promises = selectedRows.map((row) => deleteRecord(row.RateLineId));
+            Promise.all(promises)
+                .then(() => {
+                    this.showToast(this.labels.success, this.labels.removeSuccess, 'success');
+                })
+                .catch((error) => {
+                    this.showToast('Error', error.body.message, 'error');
+                })
+                .finally(() => {
+                    this.refreshTable();
+                });
+        }
+    }
+
+    showToast(title, message, variant) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: title,
+                variant: variant,
+                message: message
+            })
+        );
     }
 }
