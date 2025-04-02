a1fad6ebf8ea3547407d4a815c965b0b16a04604
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-04-02 17:21:51 +0200

    feat: lwcDCExtension extend LightningModal

--- force-app/main/default/lwc/lwcDCExtension/lwcDCExtension.js
+++ force-app/main/default/lwc/lwcDCExtension/lwcDCExtension.js
@@ -1,38 +1,49 @@
 /**
- * @description  : 
- * @author       : development@nubika.com 
-**/
-import { LightningElement, api} from 'lwc';
+ * @description  :
+ * @author       : development@nubika.com
+ **/
+import { api } from 'lwc';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import * as DCMixin from 'c/dcMixin';
 import { NavigationMixin } from 'lightning/navigation';
+import LightningModal from 'lightning/modal';
 
-
-export default class LwcDCExtension extends 
+export default class LwcDCExtension extends
     DCMixin.LWCFormMixin(
         DCMixin.LWCEventMixin(
             DCMixin.LWCDisplayMixin(
                 DCMixin.LWCControllerMixin(
-                            NavigationMixin(LightningElement)
-    )))){ 
-    
+                            NavigationMixin(LightningModal)
+    )))) {
+
     /**
      * @description Loads Lightning Web Components resources (styles and scripts) to the LWC component
      * @param {Object} resources - object containing styles and scripts urls to be loaded
      * @returns {Promise} - resolves when all resources has been loaded
      */
-    loadResources(resources){
-        try{
+    loadResources(resources) {
+        try {
             let self = this;
             let promises = [];
-            if(resources){
-                if(resources.styles) resources.styles.forEach((resourceUrl)=>{ promises.push(loadStyle(self,resourceUrl)); });
-                if(resources.scripts) resources.scripts.forEach((resourceUrl)=>{ promises.push(loadScript(self,resourceUrl)); });
+            if (resources) {
+                if (resources.styles)
+                    resources.styles.forEach((resourceUrl) => {
+                        promises.push(loadStyle(self, resourceUrl));
+                    });
+                if (resources.scripts)
+                    resources.scripts.forEach((resourceUrl) => {
+                        promises.push(loadScript(self, resourceUrl));
+                    });
                 return Promise.all(promises);
-            }else{
-                return new Promise((resolve,reject)=>{resolve(null);});
             }
-        }catch(e){ return new Promise((resolve,reject)=>{ reject(e); }); }
+            return new Promise((resolve, reject) => {
+                resolve(null);
+            });
+        } catch (e) {
+            return new Promise((resolve, reject) => {
+                reject(e);
+            });
+        }
     }
 
     /**
@@ -40,9 +51,7 @@ export default class LwcDCExtension extends
      * @param {Object} obj - the object to be parsed
      * @returns {Object} the parsed object
      */
-    parseObject(obj){
-        return obj? JSON.parse(JSON.stringify(obj)) : obj;
+    parseObject(obj) {
+        return obj ? JSON.parse(JSON.stringify(obj)) : obj;
     }
-
-
 }
