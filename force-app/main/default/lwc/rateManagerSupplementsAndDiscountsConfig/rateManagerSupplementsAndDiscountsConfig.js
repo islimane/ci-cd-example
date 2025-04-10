1453c8550e042fb7e2c6ad79ca64859650c05af0
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-04-10 13:50:49 +0200

    feat(BCRMB2B-22): Format LWC

--- force-app/main/default/lwc/rateManagerSupplementsAndDiscountsConfig/rateManagerSupplementsAndDiscountsConfig.js
+++ force-app/main/default/lwc/rateManagerSupplementsAndDiscountsConfig/rateManagerSupplementsAndDiscountsConfig.js
@@ -2,43 +2,45 @@
  * @description       :
  * @author            : Inetum Team <alberto.martinez-lopez@inetum.com>
  * @group             :
- * @last modified on  : 08-04-2025
- * @last modified by  : alberto.martinez-lopez@inetum.com
-**/
-import LwcDCExtension from 'c/lwcDCExtension';
-import { RateManagerMixin } from 'c/rateManagerMixin';
-import { RateManagerExtendedDataTableMixin } from 'c/rateManagerExtendedDataTableMixin';
-import LABELS from './labels';
-import modalSupplementsAndDiscounts from 'c/rateManagerModalSupplementsAndDiscountsHandler';
+ * @last modified on  : 10-04-2025
+ * @last modified by  : Inetum Team <ruben.sanchez-gonzalez@inetum.com>
+ **/
+import LwcDCExtension from 'c/lwcDCExtension'
+import { RateManagerMixin } from 'c/rateManagerMixin'
+import { RateManagerExtendedDataTableMixin } from 'c/rateManagerExtendedDataTableMixin'
+import LABELS from './labels'
+import modalSupplementsAndDiscounts from 'c/rateManagerModalSupplementsAndDiscountsHandler'
 
-const CONTROLLER_NAME = 'RateManagerSmntsAndDntsController';
+const CONTROLLER_NAME = 'RateManagerSmntsAndDntsController'
 
-const SUPPLEMENT_COLUMNS = [{ 
-        label: 'ACTIONS', 
+const SUPPLEMENT_COLUMNS = [
+    {
+        label: 'ACTIONS',
         fieldName: 'action',
-        type: "actions",
+        type: 'actions',
         typeAttributes: {
-            recordId: { fieldName: "id" },
+            recordId: { fieldName: 'id' },
             actions: [
-                {iconName: 'action:remove', label: 'Delete', action: 'delete'}, {iconName: 'action:clone', label: 'Clone', action: 'clone'}
+                { iconName: 'action:remove', label: 'Delete', action: 'delete' },
+                { iconName: 'action:clone', label: 'Clone', action: 'clone' }
             ]
         },
-        fixed: true, fixedWidth: 109 
+        fixed: true,
+        fixedWidth: 109
     },
     { label: 'SUPPLEMENT NAME', fieldName: 'Name', type: 'text', fixed: true, fixedWidth: 200, wrapText: true },
     { label: 'TYPE', fieldName: 'RegimenType', type: 'text', fixed: true, fixedWidth: 80, wrapText: true },
     { label: 'APPLICATION TYPE', fieldName: 'ApplicationType', type: 'text', fixed: true, fixedWidth: 200 },
     { label: 'APPLICABLE', fieldName: 'Applicable', type: 'text', fixed: true, fixedWidth: 101 },
-    { label: 'OBSERVATIONS', fieldName: 'Observations', type: 'text', fixed: true, fixedWidth: 200, wrapText: true, editable: true}
-];
+    { label: 'OBSERVATIONS', fieldName: 'Observations', type: 'text', fixed: true, fixedWidth: 200, wrapText: true, editable: true }
+]
 
 export default class RateManagerSupplementsAndDiscountsConfig extends RateManagerExtendedDataTableMixin(RateManagerMixin(LwcDCExtension)) {
-
-    labels = LABELS;
+    labels = LABELS
 
     /*** Connected callback.*/
     connectedCallback() {
-        this.setWireParams();
+        this.setWireParams()
     }
 
     /**
@@ -49,7 +51,7 @@ export default class RateManagerSupplementsAndDiscountsConfig extends RateManage
             ratePlannerId: this.parentId,
             rateId: this.rateId,
             controller: CONTROLLER_NAME
-        };
+        }
     }
 
     /**
@@ -57,50 +59,50 @@ export default class RateManagerSupplementsAndDiscountsConfig extends RateManage
      * @param {Object} response - The response object from the server.
      **/
     fetch = (response) => {
-        const fetchedRecords = response?.data?.filters && response?.data?.data;
+        const fetchedRecords = response?.data?.filters && response?.data?.data
         if (fetchedRecords) {
-            this.filters = response.data.filters;
-            this.data = response.data.data;
-            this.mixinBuildTable(SUPPLEMENT_COLUMNS);
+            this.filters = response.data.filters
+            this.data = response.data.data
+            this.mixinBuildTable(SUPPLEMENT_COLUMNS)
         } else {
-            console.warn('No records available in response');
+            console.warn('No records available in response')
         }
-    };
-
-    handleRowAction(event) {
-        console.log('handleRowAction rateManagerSupplementsAndDiscountsConfig' , event.detail);
-        this.mixinRowAction(event.detail.action, event.detail, () => {
-            this.refreshFetch();
-        });
     }
 
-    handleSave(event){
-        console.log('handleSave rateManagerSupplementsAndDiscountsConfig' , event.detail);
-        const draftValues = event.detail.draftValues;
-        this.mixinSaveRateDataRecords(draftValues, async (actionName, mappedDataRatePrice, mappedDataRateLine ) => {
-            try{
+    handleRowAction(event) {
+        console.log('handleRowAction rateManagerSupplementsAndDiscountsConfig', event.detail)
+        this.mixinRowAction(event.detail.action, event.detail, () => {
+            this.refreshFetch()
+        })
+    }
+
+    handleSave(event) {
+        console.log('handleSave rateManagerSupplementsAndDiscountsConfig', event.detail)
+        const draftValues = event.detail.draftValues
+        this.mixinSaveRateDataRecords(draftValues, async (actionName, mappedDataRatePrice, mappedDataRateLine) => {
+            try {
                 const result = await this.remoteAction({
                     controller: CONTROLLER_NAME,
                     action: actionName,
                     ratePlannerId: this.parentId,
                     ratePrices: JSON.stringify(mappedDataRatePrice),
-                    rateLines: JSON.stringify(mappedDataRateLine),
-                });
-                if(result)this.refreshFetch();
-            }catch(e){
-                console.error(e);
-            }        
-        }); 
+                    rateLines: JSON.stringify(mappedDataRateLine)
+                })
+                if (result) this.refreshFetch()
+            } catch (e) {
+                console.error(e)
+            }
+        })
     }
 
     async handleDelete() {
         // retrieve selected rows
-        let selectedRows = this.template.querySelector('c-rate-manager-data-table-browser')?.getSelectedRows();
+        let selectedRows = this.template.querySelector('c-rate-manager-data-table-browser')?.getSelectedRows()
         this.mixinDeleteRecords(selectedRows, () => {
-            this.refreshFetch();
-        });
+            this.refreshFetch()
+        })
     }
-    
+
     async handleAddSupplementsModal() {
         await modalSupplementsAndDiscounts.open({
             parentId: this.parentId,
