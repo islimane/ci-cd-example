272419e5dca0a64ee200fce8e0d00aa9a66f71d6
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-06-12 14:41:39 +0200

    feat(BCRMB2B-105): new LWC quoteManagerPriceConfig

---
+++ force-app/main/default/lwc/quoteManagerPriceConfig/quoteManagerPriceConfig.js
@@ -0,0 +1,219 @@
+import { api, wire, track } from 'lwc'
+import modalCss from '@salesforce/resourceUrl/customModal'
+import LwcDCExtension from 'c/lwcDCExtension'
+import { RateManagerMixin } from 'c/rateManagerMixin'
+import { RateManagerExtendedDataTableMixin } from 'c/rateManagerExtendedDataTableMixin'
+import { CloseActionScreenEvent } from 'lightning/actions'
+import { loadStyle } from 'lightning/platformResourceLoader'
+import LABELS from './labels'
+
+import { getPicklistValues } from 'lightning/uiObjectInfoApi'
+import ROOM_FIELD from '@salesforce/schema/Product2.Room__c'
+import REGIMEN_FIELD from '@salesforce/schema/Product2.Regimen_Type__c'
+import CHARACTERISTIC_FIELD from '@salesforce/schema/Product2.Characteristic__c'
+import APPLICABLE_FIELD from '@salesforce/schema/Product2.Applicable__c'
+
+const MASTER_RECORD_TYPE_ID = '012000000000000AAA'
+const CONTROLLER_NAME = 'QuoteManagerPriceConfigController'
+
+export default class QuoteManagerPriceConfig extends RateManagerExtendedDataTableMixin(RateManagerMixin(LwcDCExtension)) {
+    @api invoke() {
+        console.log('QuoteManagerPriceConfig invoked.')
+        this.setWireParams()
+    }
+
+    connectedCallback() {
+        loadStyle(this, modalCss)
+        if (this.recordId && !this._wireParams) {
+            this.setWireParams()
+        }
+    }
+
+    renderedCallback() {
+        this.refreshFetch()
+    }
+
+    @api recordId
+    @api weekdaySetId
+
+    labels = LABELS
+    fetchedData = []
+    _roomPicklistValues = []
+    _regimenPicklistValues = []
+    _charactPicklistValues = []
+    _applcblePicklistValues = []
+    @track roomsData = []
+    @track supplData = []
+    activeSections = ['rooms', 'supplements']
+    roomsContainer = 'roomsContainer'
+    supplContainer = 'supplementsContainer'
+
+    quotaSelected = false
+    preventChange = false
+    savingQuota = false
+
+    get roomHeaderLabel() {
+        let suffix = this.quotaSelected ? ' - Allotments' : ' - Prices'
+        return this.labels.rooms + suffix
+    }
+
+    get supplementsHeaderLabel() {
+        let suffix = ' - Prices'
+        return this.labels.supplements + suffix
+    }
+
+    async close() {
+        this.dispatchEvent(new CloseActionScreenEvent())
+    }
+
+    /**
+     * @description: Sets the wire parameters for the component.
+     **/
+    setWireParams() {
+        this._wireParams = {
+            quoteId: this.recordId,
+            weekdaySetId: this.weekdaySetId,
+            controller: CONTROLLER_NAME
+        }
+    }
+
+    /**
+     * @description: Fetch method that handles the response and processes the fetched records.
+     * @param {Object} response - The response object from the server.
+     **/
+    fetch = (response) => {
+        if (response?.success){
+            this.fetchedData = response?.data?.data
+            this.checkAllPicklistsLoaded()
+        }
+        else {
+            console.error('Error fetching data: ', response?.errorMsg || 'Unknown error')
+            this.showToast(this.labels.error, response.errorMsg, 'error', 'dismissable')
+        }
+    }
+
+    processFetchedData() {
+        if (this.fetchedData) {
+            this.roomsData = this.fetchedData.Room || []
+            this.supplData = this.fetchedData.Supplement || []
+            // modify data to add picklist values
+            ;[this.roomsData, this.supplData].forEach((dataArray) => {
+                dataArray.forEach((item) => {
+                    item.RoomLabel = this._roomPicklistValues.find((picklistVal) => picklistVal.value === item.Room)?.label
+                    item.CharactLabel = this._charactPicklistValues.find((picklistVal) => picklistVal.value === item.Characteristic)?.label
+                    item.ApplicableLabel = this._applcblePicklistValues.find((picklistVal) => picklistVal.value === item.Applicable)?.label
+                })
+            })
+
+            this.template.querySelector(`[data-id="${this.roomsContainer}"]`).style.minHeight = 3.2 * (this.roomsData.length + 1) + 'rem'
+            this.template.querySelector(`[data-id="${this.supplContainer}"]`).style.minHeight = 3.2 * (this.supplData.length + 1) + 'rem'
+        } else {
+            console.warn('No records available in response')
+        }
+    }
+
+    async handleSave(event) {
+        const draftValues = event.detail.draftValues
+        this.savingQuota = event.detail.isQuota
+        this.mixinSaveRateDataRecords(draftValues, this.callbackSave.bind(this), this.quotaSelected)
+    }
+
+    callbackSave = async (actionName, mappedDataRatePrice) => {
+        try {
+            let data = {
+                controller: CONTROLLER_NAME,
+                action: actionName,
+                quoteId: this.recordId,
+                fillQuota: this.savingQuota,
+                ratePrices: JSON.stringify(mappedDataRatePrice),
+                weekdaySetId: this.weekdaySetId
+            }
+            const result = await this.remoteAction(data)
+            this.showToast(
+                this.labels.success,
+                `${this.savingQuota ? 'Allotments' : 'Prices'} saved successfully`,
+                'success',
+                'dismissable'
+            )
+            if (result) {
+                this.refreshFetch()
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    handleClick() {
+        this.quotaSelected = !this.quotaSelected
+        this.template.querySelectorAll('c-quote-manager-rate-price-table')[0]?.toggleQuota()
+    }
+
+    checkAllPicklistsLoaded() {
+        if (
+            this.fetchedData?.length != 0 &&
+            this._roomPicklistValues.length != 0 &&
+            this._regimenPicklistValues.length != 0 &&
+            this._charactPicklistValues.length != 0 &&
+            this._applcblePicklistValues.length != 0
+        ) {
+            this.processFetchedData()
+            this.refillPeriods()
+        }
+    }
+
+    async refillPeriods() {
+        await this.template.querySelectorAll('c-quote-manager-rate-price-table').forEach((table) => {
+            table.toggleQuota()
+        })
+        await this.template.querySelectorAll('c-quote-manager-rate-price-table').forEach((table) => {
+            table.toggleQuota()
+        })
+    }
+
+    // #region Picklist wire
+    @wire(getPicklistValues, { recordTypeId: MASTER_RECORD_TYPE_ID, fieldApiName: ROOM_FIELD })
+    roomPicklistResults({ error, data }) {
+        if (data) {
+            this._roomPicklistValues = data.values.map((item) => {
+                return { label: item.label, value: item.value }
+            })
+            this.checkAllPicklistsLoaded()
+        } else if (error) {
+            console.error(error)
+        }
+    }
+    @wire(getPicklistValues, { recordTypeId: MASTER_RECORD_TYPE_ID, fieldApiName: REGIMEN_FIELD })
+    regimenPicklistResults({ error, data }) {
+        if (data) {
+            this._regimenPicklistValues = data.values.map((item) => {
+                return { label: item.label, value: item.value }
+            })
+            this.checkAllPicklistsLoaded()
+        } else if (error) {
+            console.error(error)
+        }
+    }
+    @wire(getPicklistValues, { recordTypeId: MASTER_RECORD_TYPE_ID, fieldApiName: CHARACTERISTIC_FIELD })
+    characteristicPicklistResults({ error, data }) {
+        if (data) {
+            this._charactPicklistValues = data.values.map((item) => {
+                return { label: item.label, value: item.value }
+            })
+            this.checkAllPicklistsLoaded()
+        } else if (error) {
+            console.error(error)
+        }
+    }
+    @wire(getPicklistValues, { recordTypeId: MASTER_RECORD_TYPE_ID, fieldApiName: APPLICABLE_FIELD })
+    applicablePicklistResults({ error, data }) {
+        if (data) {
+            this._applcblePicklistValues = data.values.map((item) => {
+                return { label: item.label, value: item.value }
+            })
+            this.checkAllPicklistsLoaded()
+        } else if (error) {
+            console.error(error)
+        }
+    }
+    // #endregion Picklist wire
+}
