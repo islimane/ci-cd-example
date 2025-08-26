816f2a530add1c04ea3733bafa91a976152ec648
Rubén Sánchez González <ruben.sanchez-gonzalez@inetum.com>
2025-08-26 13:44:05 +0200

    feat(BCRMB2B-105): cellType tooltip

--- force-app/main/default/lwc/dataTableWithCustomTypes/dataTableWithCustomTypes.js
+++ force-app/main/default/lwc/dataTableWithCustomTypes/dataTableWithCustomTypes.js
@@ -1,21 +1,26 @@
 /**
- * @description       : 
+ * @description       :
  * @author            : alberto.martinez-lopez@inetum.com
- * @group             : 
- * @last modified on  : 02-04-2025
- * @last modified by  : alberto.martinez-lopez@inetum.com
-**/
-import LightningDatatable from "lightning/datatable"
-import dataTableActionsTemplate from "./dataTableActionsTemplate.html"
+ * @group             :
+ * @last modified on  : 21-08-2025
+ * @last modified by  : Inetum Team <ruben.sanchez-gonzalez@inetum.com>
+ **/
+import LightningDatatable from 'lightning/datatable'
+import dataTableActionsTemplate from './dataTableActionsTemplate.html'
+import dataTableTooltipText from './dataTableTooltipText.html';
 
 export default class DatatableCustomTypes extends LightningDatatable {
     static customTypes = {
         actions: {
             template: dataTableActionsTemplate,
             standardCellLayout: true,
-            typeAttributes: ["recordId", "actions"],
+            typeAttributes: ['recordId', 'actions']
         },
+
+        tooltipText: {
+            template: dataTableTooltipText,
+            standardCellLayout: true,
+            typeAttributes: ['value', 'title']
+        }
     }
-
 }
-
